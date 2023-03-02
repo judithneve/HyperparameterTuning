a0e42beaf5e806c0d4ae6fdfc079bd2013d88df3
judithneve <judith.nevedm@gmail.com>
2023-03-02 15:56:43 +0100

    Study 2 drafts

---
+++ Study2/Study2_execute.R
@@ -0,0 +1,166 @@
+job_args <- commandArgs(trailingOnly=TRUE)
+print(job_args)
+
+##### Setup #####
+library(MASS)    # for mvrnorm
+library(dplyr)   # data wrangling
+# library(tidyr)   # pivot functions
+library(ranger)  # random forests
+library(caret)   # tuning
+library(pROC)    # AUC calculations
+source("DataSimFunctions.R")
+source("TuningFunctions.R")
+source("PerformanceMetricsFunctions.R")
+
+job_id <- job_args[1] %>% as.numeric()
+p      <- job_args[2] %>% as.numeric()
+
+start_seed <- job_id*100 + p
+set.seed(start_seed)
+
+# load in scenario + coef
+load("DGM_data/scenarios.RData")
+scenarios <- scenarios %>%
+  filter(n_pred == p)
+load("DGM_data/betas.RData")
+
+selected_scenario <- ifelse(job_id %% 6 == 0, 6, job_id %% 6)
+
+scenarios      <- scenarios[selected_scenario,]
+sample_size    <- scenarios$n
+n_prop         <- scenarios$prop_sample_size
+n_pred         <- scenarios$n_pred
+event_fraction <- scenarios$event_fraction
+betas <- betas_matrix[(betas_matrix[,"n_predictor"] == n_pred) & (betas_matrix[,"prevalence"] == event_fraction),3:5]
+
+# set size of the validation set
+large_sample <- 1e5
+
+##### Tuning setup #####
+
+# which metric are we optimising?
+metrics <- c("Deviance", "BrierScore", "LogLoss", "AUC", "CalInt", "CalSlope")
+
+# randomise the order in which metrics are run
+metrics_permutation <- sample(1:length(metrics), length(metrics))
+metrics <- metrics[metrics_permutation,]
+
+# make the grid
+tunegrid <- expand.grid(
+  mtry = 1:n_pred,
+  min.node.size = 1:10,
+  splitrule = "gini"
+)
+
+nrow_output <- length(metrics)
+out <- data.frame(
+  start_seed   = rep(start_seed,     nrow_output),
+  n            = rep(sample_size,    nrow_output),
+  n_prop       = rep(n_prop,         nrow_output),
+  p            = rep(n_pred,         nrow_output),
+  EF           = rep(event_fraction, nrow_output),
+  intercept    = rep(betas[1],       nrow_output),
+  beta         = rep(betas[1],       nrow_output),
+  gamma        = rep(betas[3],       nrow_output),
+  metric       = rep(NA,             nrow_output),
+  time         = rep(NA,             nrow_output),
+  AUC          = rep(NA,             nrow_output),
+  CalSlope     = rep(NA,             nrow_output),
+  CalIntercept = rep(NA,             nrow_output),
+  BrierScore   = rep(NA,             nrow_output),
+  LogLoss      = rep(NA,             nrow_output)
+)
+row_out <- 0
+
+# start the simulation
+##### Generate data #####
+# generate a dataset for each scenario
+# generate a validation set for each scenario
+dat     <- simulate_data(sample_size,  n_pred, event_fraction, betas)
+val_dat <- simulate_data(large_sample, n_pred, event_fraction, betas)
+
+##### Tuning procedure #####
+
+# for each metric, make a grid & tune
+for (metric in metrics) {
+  print(metric)
+  row_out <- row_out + 1
+  out[row_out,"metric"] <- metric
+  
+  # set up the CV function
+  if (metric == "Deviance") {
+    ctrl <- trainControl(
+      method = "cv",
+      number = 5,
+      summaryFunction = deviance,
+      classProbs = TRUE
+    )
+  }
+  if (metric == "BrierScore") {
+    ctrl <- trainControl(
+      method = "cv",
+      number = 5,
+      summaryFunction = brier_opt,
+      classProbs = TRUE
+    )
+  }
+  if (metric == "LogLoss") {
+    ctrl <- trainControl(
+      method = "cv",
+      number = 5,
+      summaryFunction = logloss_opt,
+      classProbs = TRUE
+    )
+  }
+  if (metric == "AUC") {
+    ctrl <- trainControl(
+      method = "cv",
+      number = 5,
+      summaryFunction = AUC_opt,
+      classProbs = TRUE
+    )
+  }
+  if (metric == "CalInt") {
+    ctrl <- trainControl(
+      method = "cv",
+      number = 5,
+      summaryFunction = cal_int,
+      classProbs = TRUE
+    )
+  }
+  if (metric == "CalSlope") {
+    ctrl <- trainControl(
+      method = "cv",
+      number = 5,
+      summaryFunction = cal_slope,
+      classProbs = TRUE
+    )
+  }
+  
+  start_tune <- Sys.time()
+  mod <- train(Y ~ .,
+               data = dat,
+               method = "ranger",
+               metric = metric,
+               maximize = FALSE,
+               trControl = ctrl,
+               tuneGrid = tunegrid)
+  end_tune   <- Sys.time()
+  print(end_tune)
+  tuning_time <- difftime(end_tune, start_tune, units = "mins")
+  
+  ##### Assess performance #####
+  
+  # fit best model on the validation set
+  pred <- predict(mod, newdata = val_dat[,-ncol(val_dat)], type = "prob")$pos
+  # evaluate the performance based on my metrics
+  perf <- performance(pred, val_dat$Y)
+  
+  out[row_out,"time"] <- tuning_time
+  out[row_out,11:15] <- perf
+}
+
+##### Save #####
+
+filename <- paste0("Study2/Data/sim/study2_run", job_id, "_", p, ".rds")
+saveRDS(out, file = filename)
--- TuningFunctions.R
+++ TuningFunctions.R
@@ -11,6 +11,79 @@ deviance <- function(data, lev = NULL, model = NULL) {
   c(Deviance = dev)
 }
 
+# own metric to optimise: Brier score
+brier_opt <- function(data, lev = NULL, model = NULL) {
+  obs  <- data$obs
+  pred <- data$pos
+  
+  brier <- brier(pred, obs)
+  
+  c(BrierScore = brier)
+}
+
+# own metric to optimise: logarithmic loss
+logloss_opt <- function(data, lev = NULL, model = NULL) {
+  obs  <- data$obs
+  pred <- data$pos
+  
+  ll <- logloss(pred, obs)
+  
+  c(LogLoss = ll)
+}
+
+# own metric to optimise: AUC
+AUC_opt <- function(data, lev = NULL, model = NULL) {
+  obs  <- data$obs
+  pred <- data$pos
+  
+  auc <- 1 - roc(response = obs, predictor = pred, quiet = TRUE)$auc
+  
+  c(AUC = auc)
+}
+
+# own metric to optimise: calibration intercept
+cal_int <- function(data, lev = NULL, model = NULL) {
+  obs  <- data$obs
+  pred <- data$pos
+  
+  if (sum(pred == 1) != 0){
+    pred[pred == 1] <- 1 - ((1-max(pred[pred != 1]))/2)
+  } 
+  
+  if (sum(pred == 0) != 0) {
+    pred[pred == 0] <- min(pred[pred != 0]) /2 
+  }
+  
+  cal_int <- glm(obs ~ 1,
+                 offset = log(pred/(1-pred)),
+                 family = "binomial") %>%
+    coef()
+  
+  c(CalInt = cal_int^2)
+}
+
+# own metric to optimise: calibration slope
+cal_slope <- function(data, lev = NULL, model = NULL) {
+  obs  <- data$obs
+  pred <- data$pos
+  
+  if (sum(pred == 1) != 0){
+    pred[pred == 1] <- 1 - ((1-max(pred[pred != 1]))/2)
+  } 
+  
+  if (sum(pred == 0) != 0) {
+    pred[pred == 0] <- min(pred[pred != 0]) /2 
+  }
+  
+  cal_sl <- glm(obs ~ log(pred/(1-pred)),
+                 family = "binomial") %>%
+    coef()[2]
+  
+  c(CalInt = cal_sl)
+}
+
+
+
 # hyperparameter tuning
 tune_hyperparameters <- function(dataset, tunegrid, sf_candidates, replace_candidates, ctrl) {
   fold_seed <- sample(1:1e8, 1)
