f58e9371c7268acb321a9c7d856c2c8ec8061ce7
judithneve <judith.nevedm@gmail.com>
2023-04-04 13:10:26 +0200

    Reorder functions

--- TuningFunctions.R
+++ TuningFunctions.R
@@ -1,3 +1,4 @@
+#### ALL STUDIES ####
 # own metric to optimise: deviance
 deviance <- function(data, lev = NULL, model = NULL) {
   obs  <- factor_to_outcome(data$obs)
@@ -11,6 +12,43 @@ deviance <- function(data, lev = NULL, model = NULL) {
   c(Deviance = dev)
 }
 
+#### STUDY 1 ####
+# hyperparameter tuning
+tune_hyperparameters <- function(dataset, tunegrid, sf_candidates, replace_candidates, ctrl) {
+  fold_seed <- sample(1:1e8, 1)
+  cat("\nFold seed:", fold_seed)
+  
+  mods <- list()
+  mod_performance <- data.frame()
+  for (sf in sf_candidates) {
+    print(sf)
+    for (r in replace_candidates) {
+      print(r)
+      length_list <- length(mods) + 1
+      set.seed(fold_seed)
+      mods[[length_list]] <- train(Y ~ .,
+                                   data = dataset,
+                                   method = "ranger",
+                                   metric = "Deviance",
+                                   maximize = FALSE,
+                                   trControl = ctrl,
+                                   tuneGrid = tunegrid,
+                                   num.trees = 500,
+                                   replace = r,
+                                   sample.fraction = sf)
+      mod_performance <- rbind(mod_performance,
+                               mods[[length_list]]$results %>%
+                                 filter(Deviance == min(Deviance, na.rm = TRUE)))
+    }
+  }
+  best_mod <- which.min(mod_performance$Deviance)
+  final_mod <- mods[[best_mod]]
+  # NOTE: COULD POSSIBLY SHORTEN TO OVERWRITE MODELS THAT ARE LESS GOOD
+  
+  return(list(model = final_mod, fold_seed = fold_seed))
+}
+
+#### STUDy 2 ####
 # own metric to optimise: Brier score
 brier_opt <- function(data, lev = NULL, model = NULL) {
   obs  <- data$obs
@@ -91,43 +129,7 @@ cal_slope <- function(data, lev = NULL, model = NULL) {
   c(CalSlope = log(cal_sl)^2)
 }
 
-
-
-# hyperparameter tuning
-tune_hyperparameters <- function(dataset, tunegrid, sf_candidates, replace_candidates, ctrl) {
-  fold_seed <- sample(1:1e8, 1)
-  cat("\nFold seed:", fold_seed)
-  
-  mods <- list()
-  mod_performance <- data.frame()
-  for (sf in sf_candidates) {
-    print(sf)
-    for (r in replace_candidates) {
-      print(r)
-      length_list <- length(mods) + 1
-      set.seed(fold_seed)
-      mods[[length_list]] <- train(Y ~ .,
-                                   data = dataset,
-                                   method = "ranger",
-                                   metric = "Deviance",
-                                   maximize = FALSE,
-                                   trControl = ctrl,
-                                   tuneGrid = tunegrid,
-                                   num.trees = 500,
-                                   replace = r,
-                                   sample.fraction = sf)
-      mod_performance <- rbind(mod_performance,
-                               mods[[length_list]]$results %>%
-                                 filter(Deviance == min(Deviance, na.rm = TRUE)))
-    }
-  }
-  best_mod <- which.min(mod_performance$Deviance)
-  final_mod <- mods[[best_mod]]
-  # NOTE: COULD POSSIBLY SHORTEN TO OVERWRITE MODELS THAT ARE LESS GOOD
-  
-  return(list(model = final_mod, fold_seed = fold_seed))
-}
-
+#### STUDY 3 ####
 # tuneRanger: optimise deviance
 dev_mbo <- function(task, model, pred, feats, extra.args) {
   obs <- factor_to_outcome(pred$data$truth)
