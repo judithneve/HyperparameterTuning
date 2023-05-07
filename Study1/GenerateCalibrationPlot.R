c3bcda3b27dd4f8c097a07e7f75b59b20919b25c
judithneve <judith.nevedm@gmail.com>
2023-05-07 17:56:29 +0200

    Final draft

--- Study1/GenerateCalibrationPlot.R
+++ Study1/GenerateCalibrationPlot.R
@@ -2,12 +2,6 @@ library(dplyr)
 library(purrr)
 library(ggplot2)
 
-dat <- list.files(path = "./Study1/Data/coords/", pattern = ".rds") %>%
-  paste0("./Study1/Data/coords/", .) %>% 
-  map(readRDS) %>% 
-  bind_rows() %>% 
-  mutate(hp_combination = ifelse(hp_combination == "", "None", hp_combination))
-
 HP.labs <- c('None',
              'mtry + min.node.size',
              'mtry + min.node.size + replace',
@@ -28,30 +22,39 @@ HP.labs.lines <- c('None',
                    'mtry + min.node.size + replace\n+ sample.fraction + splitrule')
 names(HP.labs.lines) <- HP.labs
 
-plot <- dat %>% 
-  mutate(hp_combination = factor(hp_combination, levels = names(HP.labs.lines), labels = HP.labs.lines)) %>% 
-  ggplot() + 
-  geom_line(aes(x = x, y = y, group = start_seed),
-            stat = "smooth",
-            method = stats::loess, 
-            formula = y ~ x,
-            se = F, 
-            col = "#525252",
-            linewidth = 0.25,
-            alpha = 0.5) +
-  geom_abline(slope = 1, intercept = 0, linewidth = 0.75) +
-  xlab("Estimated Probability") +
-  ylab("Observed Proportion") +
-  theme_classic() +
-  xlim(0, 1) +
-  ylim(0, 1) +
-  facet_wrap(~ hp_combination,
-             nrow = 3) +
-  theme(
-    axis.text = element_text(size = 10),
-    axis.title = element_text(size = 11),
-    strip.text = element_text(size = 11)
-  )
-
-plot_name <- paste0("Study1/Output/plot_EF_", 5, ".pdf")
-ggsave(plot_name, plot, width = 8.5, height = 8.5, units = "in")
+for (ef in c("ef1", "ef3", "ef5")) {
+  dat <- list.files(path = "./Study1/Data/coords/", pattern = ef) %>%
+    paste0("./Study1/Data/coords/", .) %>% 
+    map(readRDS) %>% 
+    bind_rows() %>% 
+    mutate(hp_combination = ifelse(hp_combination == "", "None", hp_combination))
+  
+  plot <- dat %>% 
+    mutate(hp_combination = factor(hp_combination, levels = names(HP.labs.lines), labels = HP.labs.lines)) %>% 
+    ggplot() + 
+    geom_line(aes(x = x, y = y, group = start_seed),
+              stat = "smooth",
+              method = stats::loess, 
+              formula = y ~ x,
+              se = F, 
+              col = "#525252",
+              linewidth = 0.25,
+              alpha = 0.5) +
+    geom_abline(slope = 1, intercept = 0, linewidth = 0.75) +
+    xlab("Estimated Probability") +
+    ylab("Observed Proportion") +
+    theme_classic() +
+    xlim(0, 1) +
+    ylim(0, 1) +
+    facet_wrap(~ hp_combination,
+               nrow = 3) +
+    theme(
+      axis.text = element_text(size = 10),
+      axis.title = element_text(size = 11),
+      strip.text = element_text(size = 11)
+    )
+  
+  plot_name <- paste0("Study1/Output/plot_", ef, ".pdf")
+  ggsave(plot_name, plot, width = 8.5, height = 8.5, units = "in")
+  
+}
--- Study3/GenerateCalibrationPlot.R
+++ Study3/GenerateCalibrationPlot.R
@@ -1,36 +1,38 @@
 library(dplyr)
 library(ggplot2)
 
-dat <- readRDS("Study3/Data/coords/study3_ef5.rds")
-
 alg.labs <- c("grid", "random", "mbo")
 names(alg.labs) <- c("Grid search", "Random search", "Model-based optimisation")
 alg.labs <- alg.labs[c(2, 1, 3)]
 
-plot <- dat %>% 
-  mutate(algorithm = factor(algorithm, levels = alg.labs, labels = names(alg.labs))) %>% 
-  ggplot() + 
-  geom_line(aes(x = x, y = y, group = seed),
-            stat = "smooth",
-            method = stats::loess, 
-            formula = y ~ x,
-            se = F, 
-            col = "#525252",
-            linewidth = 0.25,
-            alpha = 0.5) +
-  geom_abline(slope = 1, intercept = 0, linewidth = 0.75) +
-  xlab("Estimated Probability") +
-  ylab("Observed Proportion") +
-  theme_classic() +
-  xlim(0, 1) +
-  ylim(0, 1) +
-  facet_wrap(~ algorithm,
-             nrow = 2) +
-  theme(
-    axis.text = element_text(size = 10),
-    axis.title = element_text(size = 11),
-    strip.text = element_text(size = 11)
-  )
-
-plot_name <- paste0("Study3/Output/plot_EF_", 5, ".pdf")
-ggsave(plot_name, plot, width = 8.5, height = 8.5, units = "in")
+for (ef in c("ef1", "ef3", "ef5")) {
+  dat <- readRDS(paste0("Study3/Data/coords/study3_", ef, ".rds"))
+  
+  plot <- dat %>% 
+    mutate(algorithm = factor(algorithm, levels = alg.labs, labels = names(alg.labs))) %>% 
+    ggplot() + 
+    geom_line(aes(x = x, y = y, group = start_seed),
+              stat = "smooth",
+              method = stats::loess, 
+              formula = y ~ x,
+              se = F, 
+              col = "#525252",
+              linewidth = 0.25,
+              alpha = 0.5) +
+    geom_abline(slope = 1, intercept = 0, linewidth = 0.75) +
+    xlab("Estimated Probability") +
+    ylab("Observed Proportion") +
+    theme_classic() +
+    xlim(0, 1) +
+    ylim(0, 1) +
+    facet_wrap(~ algorithm,
+               nrow = 2) +
+    theme(
+      axis.text = element_text(size = 10),
+      axis.title = element_text(size = 11),
+      strip.text = element_text(size = 11)
+    )
+  
+  plot_name <- paste0("Study3/Output/plot_", ef, ".pdf")
+  ggsave(plot_name, plot, width = 8.5, height = 8.5, units = "in")
+}
